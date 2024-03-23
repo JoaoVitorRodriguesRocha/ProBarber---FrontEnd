efd6d3d112c78c2b67e8ad36d901def28fc638c6
marcoshjf <145942375+marcoshjf@users.noreply.github.com>
2024-03-23 15:32:45 -0300

    Update index.jsx
    alterando comentarios

--- src/componentes/login/index.jsx
+++ src/componentes/login/index.jsx
@@ -5,23 +5,22 @@ export default function Login(){
             <h1 className="logo">
                 <span className="pro">pro</span>BARBER
             </h1>
-            <div className="ajuste">/*ajuste de margin de usuario e senha e botao de login*/
-            <div className="cont_user">/*div para usuario*/
+            <div className="ajuste"> {/*ajuste de margin de usuario e senha e botao de login*/}
+            <div className="cont_user"> {/*div para usuario*/}
                 <h2 className="usuario">
                     Usuário
                 </h2>
-                <input className="inserir-usuario" type="text" placeholder="  Insira seu usuário" />/*input para colocar dados do usuario*/
+                <input className="inserir-usuario" type="text" placeholder="  Insira seu usuário" />{/*input para colocar dados do usuario*/}
             </div>
             <div className="cont_senha">
                 <h2 className="senha">
                  Senha
                 </h2>
-                <input className="inserir-senha" type="password" placeholder="  Insira sua senha" /> /*input para colocar dados senha*/
+                <input className="inserir-senha" type="password" placeholder="  Insira sua senha" /> {/*input para colocar dados senha*/}
             </div>
             </div>
             <div className="botao">
-                <button className="botao-entrar">Login</button>/*botão de login*/
+                <button className="botao-entrar">Login</button> {/*botão de login*/}
             </div>
     </div>
     )
-}
