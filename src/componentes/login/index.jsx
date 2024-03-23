71feec848390b8fc473490e690be5baadf9fe245
marcoshjf <145942375+marcoshjf@users.noreply.github.com>
2024-03-23 15:28:54 -0300

    Update index.jsx
    comentarios para melhor entendimento

--- src/componentes/login/index.jsx
+++ src/componentes/login/index.jsx
@@ -1,26 +1,26 @@
 export default function Login(){
     return(
+        /*definindo tela de login*/
         <div className="login">
             <h1 className="logo">
                 <span className="pro">pro</span>BARBER
             </h1>
-
-            <div className="ajuste">
-            <div className="cont_user">
+            <div className="ajuste">/*ajuste de margin de usuario e senha e botao de login*/
+            <div className="cont_user">/*div para usuario*/
                 <h2 className="usuario">
                     Usuário
                 </h2>
-                <input className="inserir-usuario" type="text" placeholder="  Insira seu usuário" />
+                <input className="inserir-usuario" type="text" placeholder="  Insira seu usuário" />/*input para colocar dados do usuario*/
             </div>
             <div className="cont_senha">
                 <h2 className="senha">
                  Senha
                 </h2>
-                <input className="inserir-senha" type="password" placeholder="  Insira sua senha" />
+                <input className="inserir-senha" type="password" placeholder="  Insira sua senha" /> /*input para colocar dados senha*/
             </div>
             </div>
             <div className="botao">
-                <button className="botao-entrar">Login</button>
+                <button className="botao-entrar">Login</button>/*botão de login*/
             </div>
     </div>
     )
